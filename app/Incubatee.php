a11f6856c6d2b77bece333825569d806971060be
Tongai <tongai@avochoc.com>
2018-12-03 07:46:18 +0200

    Incubatee information

---
+++ app/Incubatee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Incubatee extends Model
+{
+    //
+    protected $fillable =['first_name','last_name','email','contact_number','short_bio'];
+}
